e3bd09388761aeb2635ddc87956e95ef901abdec
MetaGols <mohannad.alhajy@gmail.com>
2022-02-25 08:09:18 +0400

    testing

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ app.use('/driverProfile',driverProfileRoute)
 app.use('/clientProfile',clientProfileRoute)
 app.use('/driverOrders',driverOrdersRoute)
 app.use('/driverNotifications',driverNotificationsRoute)
-app.use(express.static(path.join(__dirname, "./build2")))
+app.use(express.static(path.join(__dirname, "./build")))
 app.get('StyllaStays/*', (req, res) => {
-  res.sendFile(path.join(__dirname, "./build2", "index.html"))
+  res.sendFile(path.join(__dirname, "./build", "index.html"))
 });
 
 
