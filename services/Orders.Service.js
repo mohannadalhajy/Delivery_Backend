be59177d02c562e5222670694ea21a88024914ea
MetaGols <mohannad.alhajy@gmail.com>
2022-06-04 16:30:30 +0400

    notifications

--- services/Orders.Service.js
+++ services/Orders.Service.js
@@ -226,7 +226,7 @@ module.exports = {
           console.log(users)
           let tokens = users.map(user=>user.firebaseToken)
           console.log("usersusers2")
-          if(tokens.length) tokens = tokens.filter(user=>user.firebaseToken)
+          if(tokens.length) tokens = tokens.filter(user=>firebaseToken)
           console.log("usersusers3")
           if(tokens.length) await sendNewOrderNotificationToAdmins(tokens, notificationId)
           console.log("usersusers4")
