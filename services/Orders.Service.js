e4b2145d0813427e7ae533e9f2dc46970aec1005
MetaGols <mohannad.alhajy@gmail.com>
2022-07-13 23:24:23 +0400

    edits

--- services/Orders.Service.js
+++ services/Orders.Service.js
@@ -278,7 +278,7 @@ module.exports = {
           // }
           record = { ...record, startDate: new Date(), status: 0 }
           console.log("gggggggg",record.emirate)
-          record.emirate = (record.emirate + 4)%5
+          record.emirate = (Number(record.emirate) + 4)%5
           console.log("gggggggg",record.emirate)
           const order = await model.create(record).then(result => {
             return result
