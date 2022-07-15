4989f3a087eb45c76f1c4ae7e8346f0a4fefb179
MetaGols <mohannad.alhajy@gmail.com>
2022-07-15 22:49:03 +0400

    edits

--- services/Drivers/Order.Service.js
+++ services/Drivers/Order.Service.js
@@ -187,7 +187,7 @@ module.exports = {
           var result = {};
           for (var attrname in notification.dataValues) { result[attrname] = notification.dataValues[attrname]; }
           for (var attrname in order.dataValues) { result[attrname] = order.dataValues[attrname]; }
-          console.log("hhhhh", result.emirate)
+          if(result.emirate===4) result.emirate = 1
           resolve(new Response(true, result, {}));
         } catch (error) {
           reject(error)
